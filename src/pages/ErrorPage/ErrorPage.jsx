b151821cd8679cc75b5de0ded6102141ee4c5db6
Anthony <anthony.warnault@gmail.com>
2023-05-08 11:45:48 +0200

    feat: add page error

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import styles from "./ErrorPage.module.scss";
+import { ProtectedNavbar } from "../../components/Navbars/ProtectedNavbar/ProtectedNavbar";
+import { Link } from "react-router-dom";
+import { APP_LINKS } from "../../project/appLinks";
 
 const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
-  )
-}
+    <div className={styles.errorLayout}>
+      <ProtectedNavbar />
 
-export default ErrorPage
+      <section className={styles.error}>
+        <p className={styles.errorNumber}>404</p>
+        <p className={styles.errorMessage}>
+          Oups! La page que vous demandez n'existe pas.
+        </p>
+      </section>
+
+      <Link to={APP_LINKS.home} className={styles.link}>
+        Retourner sur la page d'accueil
+      </Link>
+    </div>
+  );
+};
+
+export default ErrorPage;
